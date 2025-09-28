5395904de771868132f039c80e86d4846889e3c2
junior <ljhjunior@outlook.com>
2025-09-28 16:31:46 -0300

    Ajutes pipiline

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,6 +22,18 @@ import "cypress-real-events";
 import '@shelex/cypress-allure-plugin';
 
 
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
+
+
 
 // Desabilita logs de XHR/fetch
 Cypress.on('uncaught:exception', () => false); // evita falhar por erros de terceiros
